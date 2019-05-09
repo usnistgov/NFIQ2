7b53168c49c0e3e3d3da30fa1556eda410b6674f
Ralph Lessmann <ralph.lessmann@crossmatch.com>
2019-05-09 15:49:29 +0200

    removed old build scripts and external dependencies

--- OpenCV/cmake/OpenCVGenInfoPlist.cmake
+++
@@ -1,4 +0,0 @@
-if(IOS)
-  configure_file("${OpenCV_SOURCE_DIR}/platforms/ios/Info.plist.in"
-                 "${CMAKE_BINARY_DIR}/ios/Info.plist")
-endif()
--- libFRFXLL/test/TestVectors/README.txt
+++
@@ -1,1 +0,0 @@
-This directory contains test vectors and some common functions used by unit tests.

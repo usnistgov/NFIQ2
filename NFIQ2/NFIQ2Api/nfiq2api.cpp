1504cc2e0275c1502a92be56b561732196671795
Greg Fiumara <gregory.fiumara@nist.gov>
2020-01-28 11:21:50 -0500

    Restoring whitespace changes for clean diff.

--- NFIQ2/NFIQ2Api/nfiq2api.cpp
+++ NFIQ2/NFIQ2Api/nfiq2api.cpp
@@ -71,8 +71,8 @@ extern "C"
     *evolution = version_evolution;
     *increment = version_build;
 #if CV_MAJOR_VERSION <= 2
-    const char *m = nullptr;
-    cvGetModuleInfo(nullptr, ocv, &m);
+    const char* m = nullptr;
+    cvGetModuleInfo( nullptr, ocv, &m );
 #else
     std::stringstream ss;
     ss << cv::getVersionMajor() << "." << cv::getVersionMinor() << "." << cv::getVersionRevision();
@@ -82,49 +82,49 @@ extern "C"
     *ocv = buf;
 #endif
   }
-  DLLEXPORT const char *STDCALL InitNfiq2()
+  DLLEXPORT const char* STDCALL InitNfiq2()
   {
     try
     {
-      if (g_nfiq2.get() == nullptr)
+      if( g_nfiq2.get() == nullptr )
       {
-#ifdef EMBED_RANDOMFOREST_PARAMETERS
-        g_nfiq2 = std::unique_ptr<NFIQ::NFIQ2Algorithm>(new NFIQ::NFIQ2Algorithm());
-#else
-        g_nfiq2 = std::unique_ptr<NFIQ::NFIQ2Algorithm>(new NFIQ::NFIQ2Algorithm(GetYamlFilePath(), "0xccd75820b48c19f1645ef5e9c481c592"));
-#endif
+#       ifdef EMBED_RANDOMFOREST_PARAMETERS
+        g_nfiq2 = std::unique_ptr<NFIQ::NFIQ2Algorithm>( new NFIQ::NFIQ2Algorithm() );
+#       else
+        g_nfiq2 = std::unique_ptr<NFIQ::NFIQ2Algorithm>( new NFIQ::NFIQ2Algorithm("nfiq2rf.yaml", "0xccd75820b48c19f1645ef5e9c481c592") );
+#       endif
+        return g_nfiq2->getParameterHash().c_str();
       }
-      return g_nfiq2->getParameterHash().c_str();
     }
-    catch (std::exception &exc)
+    catch( std::exception& exc )
     {
       std::cerr << "NFIQ2 ERROR => " << exc.what() << std::endl;
     }
     return nullptr;
   }
-  DLLEXPORT int STDCALL ComputeNfiq2Score(int fpos, const unsigned char *pixels, int size, int width, int height, int ppi)
+  DLLEXPORT int STDCALL ComputeNfiq2Score( int fpos, const unsigned char* pixels, int size, int width, int height, int ppi )
   {
     try
     {
-      if (g_nfiq2.get() != nullptr)
+      if( g_nfiq2.get() != nullptr )
       {
-        NFIQ::FingerprintImageData rawImage(pixels, size, width, height, fpos, ppi);
+        NFIQ::FingerprintImageData rawImage( pixels, size, width, height, fpos, ppi );
         std::list<NFIQ::ActionableQualityFeedback> actionableQuality;
         std::list<NFIQ::QualityFeatureData> featureVector;
         std::list<NFIQ::QualityFeatureSpeed> featureTimings;
-        int qualityScore = (int)g_nfiq2->computeQualityScore(rawImage,
-                                                             true, actionableQuality,
-                                                             false, featureVector,
-                                                             false, featureTimings);
+        int qualityScore = ( int )g_nfiq2->computeQualityScore( rawImage,
+                           true, actionableQuality,
+                           false, featureVector,
+                           false, featureTimings );
         return qualityScore;
       }
     }
-    catch (const NFIQ::NFIQException &exc)
+    catch( const NFIQ::NFIQException& exc )
     {
       std::cerr << "NFIQ2 ERROR => Return code [" << exc.getReturnCode() << "]: " << exc.getErrorMessage() << std::endl;
       return 255;
     }
-    catch (std::exception &exc)
+    catch( std::exception& exc )
     {
       std::cerr << "NFIQ2 ERROR => " << exc.what() << std::endl;
       return -1;
@@ -132,3 +132,5 @@ extern "C"
     return -2; // not initialized
   }
 }
+
+

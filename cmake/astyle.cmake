762c83e22bfc7a6e826acfec99ff3527cb991619
ralph.lessmann <ralph.lessmann@crossmatch.com>
2020-02-03 11:44:48 +0100

    add custom target format to trigger astyle

---
+++ cmake/astyle.cmake
@@ -0,0 +1,14 @@
+# set astyle executable
+set( ASTYLE_EXE "${DIST_PATH}/astyle${CMAKE_EXECUTABLE_SUFFIX}" )
+
+# add project folders to be "astyled"
+file(GLOB ASTYLE_NFIQ2_ALGORITHM "${ROOT_PATH}/NFIQ2/NFIQ2Api/*.cpp" "${ROOT_PATH}/NFIQ2/NFIQ2Api/*.h" ) 
+file(GLOB ASTYLE_NFIQ2_API "${ROOT_PATH}/NFIQ2/NFIQ2Api/*.cpp" "${ROOT_PATH}/NFIQ2/NFIQ2Api/*.h" ) 
+file(GLOB ASTYLE_NFIQ2_TRAINING "${ROOT_PATH}/NFIQ2/NFIQ2Api/*.cpp" "${ROOT_PATH}/NFIQ2/NFIQ2Api/*.h" ) 
+
+add_custom_target( format
+  COMMAND ${ASTYLE_EXE} --options=${ROOT_PATH}/astyle.cfg ${ASTYLE_NFIQ2_ALGORITHM} ${ASTYLE_NFIQ2_API} ${ASTYLE_NFIQ2_TRAINING}
+)
+
+
+

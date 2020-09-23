usnistgov/NFIQ2

commit b5459b19f143213c554cecef9a00c098b287129b

Replace hardcoded string headers in CLI

NFIQ2/NFIQ2Algorithm/src/tool/nfiq2_ui_log.cpp
@@ -49,22 +49,44 @@ void NFIQ2UI::Log::printScore(
   const std::list<NFIQ::QualityFeatureSpeed>& featureTimings ) const
 {
 
-  *( this->out ) << name << "," << std::to_string( fingerCode ) << "," << score
-                 << "," << errmsg << "," << quantized << "," << resampled << ",";
+  *( this->out ) << name << "," << std::to_string( fingerCode ) << ","
+                 << score << "," << errmsg << "," << quantized << ","
+                 << resampled;
+  if( this->verbose || this->speed )
+  {
+    *( this->out ) << ",";
+  }
 
   if( this->verbose )
   {
-    for( const auto& i : featureVector )
+
+    for( auto i = featureVector.begin(); i != featureVector.end(); ++i )
     {
-      *( this->out ) << std::setprecision( 5 ) << i.featureDataDouble << ",";
+      if( i != featureVector.begin() )
+      {
+        *( this->out ) << ",";
+      }
+
+      *( this->out ) << std::setprecision( 5 ) << i->featureDataDouble;
     }
+    if( this->speed )
+    {
+      *( this->out ) << ",";
+    }
+
   }
 
   if( this->speed )
   {
-    for( const auto& i : featureTimings )
+
+    for( auto i = featureTimings.begin(); i != featureTimings.end(); ++i )
     {
-      *( this->out ) << std::setprecision( 3 ) << i.featureSpeed << ",";
+      if( i != featureTimings.begin() )
+      {
+        *( this->out ) << ",";
+      }
+
+      *( this->out ) << std::setprecision( 5 ) << i->featureSpeed;
     }
   }
   *( this->out ) << "\n";
@@ -104,36 +126,41 @@ void NFIQ2UI::Log::printCSVHeader() const
                  << ","
                  << "Quantized"
                  << ","
-                 << "Re-sampled"
-                 << ",";
+                 << "Resampled";
+  if( this->verbose || this-> speed )
+  {
+    *( this->out ) << ",";
+  }
 
   if( this->verbose )
   {
-    *( this->out )
-        << "FDA_Bin10_0,FDA_Bin10_1,FDA_Bin10_2,FDA_Bin10_3,FDA_Bin10_4,FDA_"
-        "Bin10_5,FDA_Bin10_6,FDA_Bin10_7,FDA_Bin10_8,FDA_Bin10_9,FDA_Bin10_"
-        "Mean,FDA_Bin10_StdDev,FingerJetFX_MinCount_COMMinRect200x200,"
-        "FingerJetFX_MinutiaeCount,FJFXPos_Mu_MinutiaeQuality_2,FJFXPos_OCL_"
-        "MinutiaeQuality_80,ImgProcROIArea_Mean,LCS_Bin10_0,LCS_Bin10_1,LCS_"
-        "Bin10_2,LCS_Bin10_3,LCS_Bin10_4,LCS_Bin10_5,LCS_Bin10_6,LCS_Bin10_"
-        "7,LCS_Bin10_8,LCS_Bin10_9,LCS_Bin10_Mean,LCS_Bin10_StdDev,MMB,Mu,"
-        "OCL_Bin10_0,OCL_Bin10_1,OCL_Bin10_2,OCL_Bin10_3,OCL_Bin10_4,OCL_"
-        "Bin10_5,OCL_Bin10_6,OCL_Bin10_7,OCL_Bin10_8,OCL_Bin10_9,OCL_Bin10_"
-        "Mean,OCL_Bin10_StdDev,OF_Bin10_0,OF_Bin10_1,OF_Bin10_2,OF_Bin10_3,"
-        "OF_Bin10_4,OF_Bin10_5,OF_Bin10_6,OF_Bin10_7,OF_Bin10_8,OF_Bin10_9,"
-        "OF_Bin10_Mean,OF_Bin10_StdDev,OrientationMap_ROIFilter_"
-        "CoherenceRel,OrientationMap_ROIFilter_CoherenceSum,RVUP_Bin10_0,"
-        "RVUP_Bin10_1,RVUP_Bin10_2,RVUP_Bin10_3,RVUP_Bin10_4,RVUP_Bin10_5,"
-        "RVUP_Bin10_6,RVUP_Bin10_7,RVUP_Bin10_8,RVUP_Bin10_9,RVUP_Bin10_"
-        "Mean,RVUP_Bin10_StdDev,";
+    std::vector<std::string> vHeaders =
+      NFIQ::NFIQ2Algorithm::getAllQualityFeatureIDs();
+
+    for( auto it = vHeaders.begin(); it != vHeaders.end(); ++it )
+    {
+      if( it != vHeaders.begin() )
+      {
+        *( this->out ) << ',';
+      }
+      *( this->out ) << *it;
+    }
   }
 
   if( this->speed )
   {
-    *( this->out )
-        << "Contrast,Frequency domain,Minutiae,Minutiae quality,Region of "
-        "interest,Local clarity,Orientation certainty,Orientation "
-        "flow,Quality map,Ridge valley uniformity,";
+    std::vector<std::string> sHeaders =
+      NFIQ::NFIQ2Algorithm::getAllSpeedFeatureGroups();
+
+    for( auto it = sHeaders.begin(); it != sHeaders.end(); ++it )
+    {
+      if( it != sHeaders.begin() )
+      {
+        *( this->out ) << ',';
+      }
+      std::replace( it->begin(), it->end(), ' ', '_' );
+      *( this->out ) << *it;
+    }
   }
   *( this->out ) << "\n";
 }
@@ -142,33 +169,3 @@ NFIQ2UI::Log::~Log()
 {
   this->out = nullptr;
 }
-
-// -- Code will try to be used in the future
-// when changes to NFIQ2 allow for headers to get
-// collected before an image is processed --
-
-/*
-void NFIQ2UI::Log::printCSVHeader(
-  std::list<NFIQ::QualityFeatureData> featureVector,
-  std::list<NFIQ::QualityFeatureSpeed> featureTimings) {
-
- *(this->out) << "Filename" << ","
-               << "QualityScore" << ","
-               << "OptionalError" << ","
-               << "Quantized" << ","
-               << "Re-sampled" << ",";
-
-  if (this->verboseFlag) {
-    for (const auto i : featureVector){
-      *(this->out) << i.featureID << ",";
-    }
-  }
-
-  if (this->speedFlag) {
-    for (const auto i : featureTimings){
-      *(this->out) << i.featureIDGroup << ",";
-    }
-  }
-  *(this->out)  << "\n";
-}
-*/

NFIQ2/NFIQ2Algorithm/include/NFIQ2Algorithm.h
@@ -58,6 +58,24 @@ namespace NFIQ
 
       /**
        * @brief
+       * Obtain all quality feature IDs from quality modules.
+       *
+       * @return
+       * Vector of strings containing all quality feature IDs.
+       */
+      static std::vector<std::string> getAllQualityFeatureIDs();
+
+      /**
+       * @brief
+       * Obtain all speed feature groups from quality modules.
+       *
+       * @return
+       * Vector of strings containing all speed feature groups.
+       */
+      static std::vector<std::string> getAllSpeedFeatureGroups();
+
+      /**
+       * @brief
        * Obtain MD5 checksum of Random Forest parameter file loaded.
        *
        * @return

NFIQ2/NFIQ2Algorithm/include/features/FDAFeature.h
@@ -37,7 +37,8 @@ class FDAFeature : BaseFeature
     {
     };
 
-    virtual std::list<std::string> getAllFeatureIDs();
+    static std::list<std::string> getAllFeatureIDs();
+    static const std::string speedFeatureIDGroup;
 
   private:
     int blocksize;

NFIQ2/NFIQ2Algorithm/include/features/FJFXMinutiaeQualityFeatures.h
@@ -69,7 +69,8 @@ class FJFXMinutiaeQualityFeature : BaseFeature
 
     void initModule() { /* not needed here */ };
 
-    std::list<std::string> getAllFeatureIDs();
+    static std::list<std::string> getAllFeatureIDs();
+    static const std::string speedFeatureIDGroup;
 
   private:
 

NFIQ2/NFIQ2Algorithm/include/features/FingerJetFXFeature.h
@@ -111,7 +111,8 @@ class FingerJetFXFeature : BaseFeature
 
     void initModule() { /* not needed here */ };
 
-    std::list<std::string> getAllFeatureIDs();
+    static std::list<std::string> getAllFeatureIDs();
+    static const std::string speedFeatureIDGroup;
 
   private:
     FRFXLL_RESULT createContext( FRFXLL_HANDLE_PT phContext ) ;

NFIQ2/NFIQ2Algorithm/include/features/ImgProcROIFeature.h
@@ -45,7 +45,8 @@ class ImgProcROIFeature : BaseFeature
 
     void initModule() { /* not needed here */ };
 
-    std::list<std::string> getAllFeatureIDs();
+    static std::list<std::string> getAllFeatureIDs();
+    static const std::string speedFeatureIDGroup;
 
     static ImgProcROIResults computeROI( cv::Mat& img, unsigned int bs );
 

NFIQ2/NFIQ2Algorithm/include/features/LCSFeature.h
@@ -31,7 +31,8 @@ class LCSFeature : BaseFeature
 
     virtual void initModule() {};
 
-    virtual std::list<std::string> getAllFeatureIDs();
+    static std::list<std::string> getAllFeatureIDs();
+    static const std::string speedFeatureIDGroup;
 
   protected:
     int blocksize;

NFIQ2/NFIQ2Algorithm/include/features/MuFeature.h
@@ -31,7 +31,8 @@ class MuFeature : BaseFeature
 
     void initModule() { /* not needed here */ };
 
-    std::list<std::string> getAllFeatureIDs();
+    static std::list<std::string> getAllFeatureIDs();
+    static const std::string speedFeatureIDGroup;
 };
 
 

commit e7faa5f148bd71cf4f514c352a1d4870dc3904ec

Add missing comma separator.
Regression since b5459b19f143213c554cecef9a00c098b287129b. Occured
with both -q and -v options enabled.

NFIQ2/NFIQ2Algorithm/src/tool/nfiq2_ui_log.cpp
@@ -145,6 +145,11 @@ void NFIQ2UI::Log::printCSVHeader() const
       }
       *( this->out ) << *it;
     }
+
+    if( this->speed )
+    {
+      *( this->out ) << ',';
+    }
   }
 
   if( this->speed )
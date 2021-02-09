9c6d8acbbfd7695bfdd011546ef912555e5a9502
aef1 <andrew.figlarz@nist.gov>
2021-02-09 10:53:56 -0500

    Reverting 93ee91f

--- NFIQ2/NFIQ2Api/nfiq2api.cpp
+++ NFIQ2/NFIQ2Api/nfiq2api.cpp
@@ -80,9 +80,9 @@ GetNfiq2Version(int *major, int *minor, int *patch, const char **ocv)
 	*ocv = buf;
 #endif
 }
-// Require an allocated buffer of size 33 to accomodate md5 hash
 DLLEXPORT const char *STDCALL
-InitNfiq2(char *paramHash)
+
+InitNfiq2()
 {
 	try {
 		if (g_nfiq2.get() == nullptr) {
@@ -94,9 +94,7 @@ InitNfiq2(char *paramHash)
 			    new NFIQ::NFIQ2Algorithm(GetYamlFilePath(),
 				"ccd75820b48c19f1645ef5e9c481c592"));
 #endif
-			strncpy(
-			    paramHash, g_nfiq2->getParameterHash().c_str(), 33);
-			return paramHash;
+			return g_nfiq2->getParameterHash().c_str();
 		}
 	} catch (std::exception &exc) {
 		std::cerr << "NFIQ2 ERROR => " << exc.what() << std::endl;

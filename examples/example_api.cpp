843421712bafbffca3f0cde2fb54aea5a13f9a64
aef1 <andrew.figlarz@nist.gov>
2021-03-24 15:32:02 -0400

    Added headers to be printed out in sample app

--- examples/example_api.cpp
+++ examples/example_api.cpp
@@ -1,14 +1,22 @@
 #include <nfiq2/nfiq2.hpp>
+#include <nfiq2/nfiq2_qualityfeatures.hpp>
 #include <opencv2/core/mat.hpp>
 #include <opencv2/imgcodecs.hpp>
 
 #include <iostream>
+#include <memory>
 
 int
 main(int argc, char **argv)
 {
 	if (argc != 3) {
-		std::cout << "Incorrect Number of Arguments\n";
+		std::cout
+		    << "NFIQ 2 API Example\n\nThis is a sample program that "
+		       "shows how to use the NFIQ 2 API on an image.\n\nThis command line "
+		       "tool takes two arguments.\nThe first is the path to a NFIQ 2 "
+		       "RandomForest model information file.\nThe second is the path to a "
+		       "single fingerprint image.\n\nPlease provide arguments to the binary "
+		       "in the designated order.\n";
 		return (EXIT_FAILURE);
 	}
 
@@ -17,13 +25,14 @@ main(int argc, char **argv)
 		modelInfoObj = NFIQ::ModelInfo(argv[1]);
 	} catch (...) {
 		std::cout
-		    << "Could not parse model info file. Ensure it is the first argument on the cmd line\n";
+		    << "Could not parse model info file. "
+		       "Ensure it is the first argument on the cmd line\n";
 		return (EXIT_FAILURE);
 	}
 
-	std::shared_ptr<NFIQ::NFIQ2Algorithm> model {};
+	std::unique_ptr<NFIQ::NFIQ2Algorithm> model {};
 	try {
-		model = std::make_shared<NFIQ::NFIQ2Algorithm>(modelInfoObj);
+		model.reset(new NFIQ::NFIQ2Algorithm(modelInfoObj));
 	} catch (...) {
 		std::cout
 		    << "Could not initialize model from model info file\n";
@@ -35,10 +44,17 @@ main(int argc, char **argv)
 		imgMat = cv::imread(argv[2], cv::IMREAD_GRAYSCALE);
 	} catch (...) {
 		std::cout
-		    << "Could not parse image file. Ensure it is the second argument on the cmd line\n";
+		    << "Could not parse image file. Ensure it is the second "
+		       "argument on the cmd line\n";
 		return (EXIT_FAILURE);
 	}
 
+	// Note: NFIQ 2 only operates on images captured at a resolution of
+	// 500 PPI. Unfortunately, the images contained within this repository
+	// are encoded in PGM format, which does not encode resolution
+	// information. ALWAYS make use of image libraries to decode image
+	// resolution information and convert to PPI to avoid erroneous NFIQ 2
+	// results.
 	NFIQ::FingerprintImageData rawImage = NFIQ::FingerprintImageData(
 	    imgMat.data, static_cast<uint32_t>(imgMat.total()),
 	    static_cast<uint32_t>(imgMat.cols),
@@ -52,21 +68,37 @@ main(int argc, char **argv)
 		return (EXIT_FAILURE);
 	}
 
-	std::cout << "NFIQ 2 Quality Score: " << results.getScore() << "\n";
+	std::cout << "QualityScore: " << results.getScore() << "\n";
 
-	for (const auto &i : results.getActionable()) {
-		std::cout << "Actionable Score: " << i.actionableQualityValue
+	std::vector<std::string> actionableHeaders =
+	    NFIQ::QualityFeatures::getAllActionableIdentifiers();
+	std::vector<NFIQ::ActionableQualityFeedback> actionableResults =
+	    results.getActionable();
+
+	for (size_t i = 0; i < actionableHeaders.size(); i++) {
+		std::cout << actionableHeaders.at(i) << ": "
+			  << actionableResults.at(i).actionableQualityValue
 			  << "\n";
 	}
 
-	for (const auto &i : results.getQuality()) {
-		std::cout << "Feature Quality Score: " << i.featureDataDouble
-			  << "\n";
+	std::vector<std::string> featureHeaders =
+	    NFIQ::QualityFeatures::getAllQualityFeatureIDs();
+	std::vector<NFIQ::QualityFeatureData> featureResults =
+	    results.getQuality();
+
+	for (size_t i = 0; i < featureHeaders.size(); i++) {
+		std::cout << featureHeaders.at(i) << ": "
+			  << featureResults.at(i).featureDataDouble << "\n";
 	}
 
-	for (const auto &i : results.getSpeed()) {
-		std::cout << "Feature Quality Speed: " << i.featureSpeed
-			  << "\n";
+	std::vector<std::string> speedHeaders =
+	    NFIQ::QualityFeatures::getAllSpeedFeatureGroups();
+	std::vector<NFIQ::QualityFeatureSpeed> speedResults =
+	    results.getSpeed();
+
+	for (size_t i = 0; i < speedHeaders.size(); i++) {
+		std::cout << speedHeaders.at(i) << ": "
+			  << speedResults.at(i).featureSpeed << "\n";
 	}
 
 	return (EXIT_SUCCESS);

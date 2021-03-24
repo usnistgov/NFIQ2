66c5ad2b972b843e9189190ab0982338b3bc9cea
Andrew Figlarz <32622701+afiglarz@users.noreply.github.com>
2021-03-24 13:20:00 -0400

    Update NFIQ2/NFIQ2Algorithm/include/nfiq2/nfiqexception.hpp
    Co-authored-by: Greg Fiumara <gregory.fiumara@nist.gov>

--- NFIQ2/NFIQ2Algorithm/include/nfiq2/nfiqexception.hpp
+++ NFIQ2/NFIQ2Algorithm/include/nfiq2/nfiqexception.hpp
@@ -52,7 +52,7 @@ typedef enum error_code_e {
 	e_Error_FeatureCalculationError_FJFX_CannotGetMinutiaeRecords = 51,
 	e_Error_FeatureCalculationError_FJFX_NoMinutiaeFound = 52,
 	e_Error_FeatureCalculationError_FJFX_CannotGetMinutiaeData = 53,
-	// Scores must be between 1 and 100
+	// Scores must be between 0 and 100
 	e_Error_InvalidNFIQ2Score = 54
 } ErrorCode;
 

6cb60e4f86e8085855b7f7b959a9faf1ea602e35
Greg Fiumara <gregory.fiumara@nist.gov>
2021-04-22 11:46:05 -0400

    Add universal header

---
+++ NFIQ2/NFIQ2Algorithm/include/nfiq2.hpp
@@ -0,0 +1,25 @@
+/*
+ * This file is part of NIST Fingerprint Image Quality (NFIQ) 2. For more
+ * information on this project, refer to:
+ *   - https://nist.gov/services-resources/software/nfiq2
+ *   - https://github.com/usnistgov/NFIQ2
+ *
+ * This work is in the public domain. For complete licensing details, refer to:
+ *   - https://github.com/usnistgov/NFIQ2/blob/master/LICENSE.md
+ */
+
+#ifndef NFIQ2_HPP_
+#define NFIQ2_HPP_
+
+#include <nfiq2_data.hpp>
+#include <nfiq2_fingerprintimagedata.hpp>
+#include <nfiq2_interfacedefinitions.hpp>
+#include <nfiq2_modelinfo.hpp>
+#include <nfiq2_nfiq2algorithm.hpp>
+#include <nfiq2_nfiqexception.hpp>
+#include <nfiq2_qualityfeatures.hpp>
+#include <nfiq2_results.hpp>
+#include <nfiq2_timer.hpp>
+#include <nfiq2_version.hpp>
+
+#endif /* NFIQ2_HPP_ */

dccc01954be19e22d3eddfa13a77ac45d27271b4
Greg Fiumara <gregory.fiumara@nist.gov>
2021-04-22 14:21:22 -0400

    Replace OS-specific timer with C++ chrono.
    Move implementations into separate source file.

---
+++ NFIQ2/NFIQ2Algorithm/src/nfiq2/nfiq2_timer.cpp
@@ -0,0 +1,31 @@
+#include <nfiq2_timer.hpp>
+
+#include <chrono>
+
+void
+NFIQ::Timer::startTimer()
+{
+	this->endTime = std::chrono::steady_clock::time_point {};
+	this->startTime = std::chrono::steady_clock::now();
+}
+
+void
+NFIQ::Timer::endTimer()
+{
+	this->endTime = std::chrono::steady_clock::now();
+}
+
+double
+NFIQ::Timer::getElapsedTime()
+{
+	return std::chrono::duration<double, std::milli>(
+	    this->endTime - this->startTime)
+	    .count();
+}
+
+double
+NFIQ::Timer::endTimerAndGetElapsedTime()
+{
+	this->endTime = std::chrono::steady_clock::now();
+	return this->getElapsedTime();
+}

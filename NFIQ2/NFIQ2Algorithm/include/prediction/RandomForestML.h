f750028f94948fd8733fceab8cd198f28e14e72e
aef1 <andrew.figlarz@nist.gov>
2021-05-03 10:59:40 -0400

    Adding RandomForestML documentation

--- NFIQ2/NFIQ2Algorithm/include/prediction/RandomForestML.h
+++ NFIQ2/NFIQ2Algorithm/include/prediction/RandomForestML.h
@@ -12,20 +12,48 @@
 
 namespace NFIQ2 { namespace Prediction {
 
+/**
+ * @class RandomForestML
+ * @brief This class handles the Random Forest Machine Learning model used
+ * to generate NFIQ 2 quality scores.
+ */
 class RandomForestML {
+
     public:
+	/**
+	 * @brief Default constructor of RandomForestML
+	 */
 	RandomForestML();
+	/**
+	 * @brief Destructor
+	 */
 	virtual ~RandomForestML();
 
+	/** Stores the module name of the random RandomForestML object */
 	static const std::string moduleName;
+
+	/**
+	 * @brief Returns the Random Forest Module name
+	 */
 	std::string getModuleName() const;
 
 #ifdef EMBED_RANDOMFOREST_PARAMETERS
+	/**
+	 * @brief Initialize model (When using embedded parameters)
+	 */
 	std::string initModule();
 #endif
+
+	/**
+	 * @brief Initialize model (When not using embedded parameters)
+	 */
 	std::string initModule(
 	    const std::string &fileName, const std::string &fileHash);
 
+	/**
+	 * @brief Compute NFIQ2 quality score based on model and provided
+	 * QualityFeatureData
+	 */
 	void evaluate(
 	    const std::unordered_map<std::string, NFIQ2::QualityFeatureData>
 		&features,
@@ -33,11 +61,15 @@ class RandomForestML {
 	    double &deviation) const;
 
     private:
+	/** OpenCV shared smart pointer refering to the RF model itself */
 	cv::Ptr<cv::ml::RTrees> m_pTrainedRF;
-
+	/** Calculates the hash of the RandomForest parameters */
 	std::string calculateHashString(const std::string &s);
+	/** Initialize model using string parameters */
 	void initModule(const std::string &params);
+
 #ifdef EMBED_RANDOMFOREST_PARAMETERS
+	/** Extracts string parameters when model is embedded */
 	std::string joinRFTrainedParamsString();
 #endif
 };

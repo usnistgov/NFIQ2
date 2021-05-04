usnistgov/NFIQ2

commit ff3cc1e7f71672d4eb6cbc63401f8cbec1d94642

Add default constructor for Algorithm
* Update example API to not use smart pointer

Co-authored-by: Greg Fiumara <gregory.fiumara@nist.gov>

NFIQ2/NFIQ2Algorithm/src/nfiq2/nfiq2_algorithm_impl.cpp
@@ -12,11 +12,9 @@
 #include <nfiq2_fingerprintimagedata.hpp>
 #include <nfiq2_qualityfeatures.hpp>
 #include <nfiq2_timer.hpp>
-#include <string.h>
 
 #include "nfiq2_algorithm_impl.hpp"
 #include <iomanip>
-#include <list>
 #include <string>
 #include <vector>
 
@@ -28,27 +26,29 @@ set_fpu(unsigned int mode)
 }
 #endif
 
-#ifdef EMBED_RANDOMFOREST_PARAMETERS
 NFIQ2::Algorithm::Impl::Impl()
+    : initialized { false }
 {
 #if defined(__linux) && defined(__i386__)
 	set_fpu(0x27F); /* use double-precision rounding */
 #endif
+
+#ifdef EMBED_RANDOMFOREST_PARAMETERS
 	// init RF module that takes some time to load the parameters
 	this->m_parameterHash = m_RandomForestML.initModule();
-}
+	this->initialized = true;
 #endif
+}
 
 NFIQ2::Algorithm::Impl::Impl(
     const std::string &fileName, const std::string &fileHash)
+    : Impl()
 {
-#if defined(__linux) && defined(__i386__)
-	set_fpu(0x27F); /* use double-precision rounding */
-#endif
 	// init RF module that takes some time to load the parameters
 	try {
 		this->m_parameterHash = m_RandomForestML.initModule(
 		    fileName, fileHash);
+		this->initialized = true;
 	} catch (const cv::Exception &e) {
 		throw Exception(NFIQ2::ErrorCode::BadArguments,
 		    "Could not initialize random forest parameters with "
@@ -66,15 +66,15 @@ NFIQ2::Algorithm::Impl::Impl(
 	}
 }
 
-NFIQ2::Algorithm::Impl::~Impl()
-{
-}
+NFIQ2::Algorithm::Impl::~Impl() = default;
 
 double
 NFIQ2::Algorithm::Impl::getQualityPrediction(
     const std::unordered_map<std::string, NFIQ2::QualityFeatureData> &features)
     const
 {
+	this->throwIfUninitialized();
+
 	const double utility {};
 	double deviation {};
 	double quality {};
@@ -88,6 +88,7 @@ NFIQ2::Algorithm::Impl::computeQualityScore(
     const std::vector<std::shared_ptr<NFIQ2::QualityFeatures::BaseFeature>>
 	&features) const
 {
+	this->throwIfUninitialized();
 
 	const std::unordered_map<std::string, NFIQ2::QualityFeatureData>
 	    quality = NFIQ2::QualityFeatures::getQualityFeatureData(features);
@@ -117,6 +118,7 @@ unsigned int
 NFIQ2::Algorithm::Impl::computeQualityScore(
     const NFIQ2::FingerprintImageData &rawImage) const
 {
+	this->throwIfUninitialized();
 
 	// --------------------------------------------------------
 	// compute quality features (including actionable feedback)
@@ -167,11 +169,29 @@ NFIQ2::Algorithm::Impl::computeQualityScore(
     const std::unordered_map<std::string, NFIQ2::QualityFeatureData> &features)
     const
 {
+	this->throwIfUninitialized();
+
 	return (unsigned int)getQualityPrediction(features);
 }
 
 std::string
 NFIQ2::Algorithm::Impl::getParameterHash() const
 {
+	this->throwIfUninitialized();
+
 	return (this->m_parameterHash);
+}
+
+void
+NFIQ2::Algorithm::Impl::throwIfUninitialized() const
+{
+	if (!this->isInitialized())
+		throw NFIQ2::Exception { NFIQ2::ErrorCode::MachineLearningError,
+			"Random forest parameters were not loaded" };
+}
+
+bool
+NFIQ2::Algorithm::Impl::isInitialized() const
+{
+	return (this->initialized);
 }

NFIQ2/NFIQ2Algorithm/include/nfiq2_algorithm.hpp
@@ -14,11 +14,15 @@ namespace NFIQ2 {
 /** Wrapper to return quality scores for a fingerprint image */
 class Algorithm {
     public:
-#ifdef EMBED_RANDOMFOREST_PARAMETERS
 	Algorithm();
-#endif
 	Algorithm(const std::string &fileName, const std::string &fileHash);
 	Algorithm(const NFIQ2::ModelInfo &modelInfoObj);
+
+	Algorithm(const Algorithm &);
+	Algorithm &operator=(const Algorithm &);
+
+	Algorithm(Algorithm &&) noexcept;
+	Algorithm &operator=(Algorithm &&) noexcept;
 	~Algorithm();
 
 	/**
@@ -27,6 +31,8 @@ class Algorithm {
 	 * data
 	 * @param rawImage fingerprint image in raw format
 	 * @return achieved quality score
+	 * @throw Exception
+	 * Called before random forest parameters were loaded
 	 */
 	unsigned int computeQualityScore(
 	    const NFIQ2::FingerprintImageData &rawImage) const;
@@ -38,6 +44,8 @@ class Algorithm {
 	 * @param features list of computed feature metrics that contain quality
 	 * information for a fingerprint image
 	 * @return achieved quality score
+	 * @throw Exception
+	 * Called before random forest parameters were loaded
 	 */
 	unsigned int computeQualityScore(const std::vector<
 	    std::shared_ptr<NFIQ2::QualityFeatures::BaseFeature>> &features)
@@ -49,6 +57,8 @@ class Algorithm {
 	 * quality feature data
 	 * @param features map of string, quality feature data pairs
 	 * @return achieved quality score
+	 * @throw Exception
+	 * Called before random forest parameters were loaded
 	 */
 	unsigned int computeQualityScore(
 	    const std::unordered_map<std::string, NFIQ2::QualityFeatureData>
@@ -60,15 +70,24 @@ class Algorithm {
 	 *
 	 * @return
 	 * MD5 checksum of the Random Forest parameter file loaded.
+	 * @throw Exception
+	 * Called before random forest parameters were loaded.
 	 */
 	std::string getParameterHash() const;
 
+	/**
+	 * @brief
+	 * Determine if random forest parameters have been loaded.
+	 *
+	 * @return
+	 * true if some set of random forest parameters have been loaded, false
+	 * otherwise.
+	 */
+	bool isInitialized() const;
+
     private:
 	class Impl;
-	const std::unique_ptr<const Algorithm::Impl> pimpl;
-
-	Algorithm(const Algorithm &) = delete;
-	Algorithm &operator=(const Algorithm &) = delete;
+	std::unique_ptr<Algorithm::Impl> pimpl;
 };
 } // namespace NFIQ
 

NFIQ2/NFIQ2Algorithm/src/nfiq2/nfiq2_algorithm.cpp
@@ -3,12 +3,10 @@
 
 #include "nfiq2_algorithm_impl.hpp"
 
-#ifdef EMBED_RANDOMFOREST_PARAMETERS
 NFIQ2::Algorithm::Algorithm()
     : pimpl { new NFIQ2::Algorithm::Impl() }
 {
 }
-#endif
 
 NFIQ2::Algorithm::Algorithm(
     const std::string &fileName, const std::string &fileHash)
@@ -20,6 +18,18 @@ NFIQ2::Algorithm::Algorithm(const NFIQ2::ModelInfo &modelInfoObj)
     : NFIQ2::Algorithm { modelInfoObj.getModelPath(),
 	    modelInfoObj.getModelHash() }
 {
+}
+
+NFIQ2::Algorithm::Algorithm(const Algorithm &rhs)
+    : pimpl(new Impl(*rhs.pimpl))
+{
+}
+
+NFIQ2::Algorithm &
+NFIQ2::Algorithm::operator=(const Algorithm &rhs)
+{
+	*pimpl = *rhs.pimpl;
+	return *this;
 }
 
 unsigned int
@@ -51,4 +61,12 @@ NFIQ2::Algorithm::getParameterHash() const
 	return (this->pimpl->getParameterHash());
 }
 
+bool
+NFIQ2::Algorithm::isInitialized() const
+{
+	return (this->pimpl->isInitialized());
+}
+
 NFIQ2::Algorithm::~Algorithm() = default;
+NFIQ2::Algorithm::Algorithm(NFIQ2::Algorithm &&) noexcept = default;
+NFIQ2::Algorithm &NFIQ2::Algorithm::operator=(Algorithm &&) noexcept = default;

NFIQ2/NFIQ2Algorithm/src/nfiq2/nfiq2_algorithm_impl.hpp
@@ -28,9 +28,7 @@ class Algorithm::Impl {
 	/**
 	 * @brief Default constructor of Impl
 	 */
-#ifdef EMBED_RANDOMFOREST_PARAMETERS
 	Impl();
-#endif
 	Impl(const std::string &fileName, const std::string &fileHash);
 
 	/**
@@ -48,6 +46,8 @@ class Algorithm::Impl {
 	 * data
 	 * @param rawImage fingerprint image in raw format
 	 * @return achieved quality score
+	 * @throw Exception
+	 * Called before random forest parameters were loaded
 	 */
 	unsigned int computeQualityScore(
 	    const NFIQ2::FingerprintImageData &rawImage) const;
@@ -59,6 +59,8 @@ class Algorithm::Impl {
 	 * @param features list of computed feature metrics that contain quality
 	 * information for a fingerprint image
 	 * @return achieved quality score
+	 * @throw Exception
+	 * Called before random forest parameters were loaded
 	 */
 	unsigned int computeQualityScore(const std::vector<
 	    std::shared_ptr<NFIQ2::QualityFeatures::BaseFeature>> &features)
@@ -70,6 +72,8 @@ class Algorithm::Impl {
 	 * quality feature data
 	 * @param features map of string, quality feature data pairs
 	 * @return achieved quality score
+	 * @throw Exception
+	 * Called before random forest parameters were loaded
 	 */
 	unsigned int computeQualityScore(
 	    const std::unordered_map<std::string, NFIQ2::QualityFeatureData>
@@ -81,17 +85,35 @@ class Algorithm::Impl {
 	 *
 	 * @return
 	 * MD5 checksum of the Random Forest parameter file loaded.
+	 *
+	 * @throw Exception
+	 * Called before random forest parameters were loaded
 	 */
 	std::string getParameterHash() const;
 
+	/** @return Whether or not random forest parameters have been loaded. */
+	bool isInitialized() const;
+
     private:
+	/** Whether or not random forest parameters have been loaded. */
+	bool initialized { false };
 	/**
 	 * @throws Exception
-	 * Failure to compute (OpenCV reason contained within message string).
+	 * Failure to compute (OpenCV reason contained within message string) or
+	 * called before random forest parameters loaded.
 	 */
 	double getQualityPrediction(
 	    const std::unordered_map<std::string, NFIQ2::QualityFeatureData>
 		&features) const;
+
+	/**
+	 * @brief
+	 * Throw an exception if random forest parameters have not been loaded.
+	 *
+	 * @throw NFIQ2::Exception
+	 * Random forest parameters have not been loaded.
+	 */
+	void throwIfUninitialized() const;
 
 	NFIQ2::Prediction::RandomForestML m_RandomForestML;
 	std::string m_parameterHash {};

examples/example_api.cpp
@@ -66,9 +66,9 @@ main(int argc, char **argv)
 
 	// This segment loads the model into memory so that it can be used to
 	// generate a NFIQ 2 score.
-	std::unique_ptr<NFIQ2::Algorithm> model {};
+	NFIQ2::Algorithm model {};
 	try {
-		model.reset(new NFIQ2::Algorithm(modelInfoObj));
+		model = NFIQ2::Algorithm(modelInfoObj);
 	} catch (...) {
 		std::cout
 		    << "Could not initialize model from model info file\n";
@@ -111,7 +111,7 @@ main(int argc, char **argv)
 	// NFIQ 2 quality score
 	unsigned int nfiq2 {};
 	try {
-		nfiq2 = model->computeQualityScore(features);
+		nfiq2 = model.computeQualityScore(features);
 	} catch (...) {
 		std::cout << "Error in calculating NFIQ 2 score\n";
 		return (EXIT_FAILURE);

commit ec2bad17f8c4c788a047fe9dab82fda721ef57e5

Don't delegate constructor.
In the embedded parameters case, this would result in extra work.
Instead, do the shared work directly.

Regression since ff3cc1e7f71672d4eb6cbc63401f8cbec1d94642.

NFIQ2/NFIQ2Algorithm/src/nfiq2/nfiq2_algorithm_impl.cpp
@@ -30,7 +30,7 @@ NFIQ2::Algorithm::Impl::Impl()
 
 NFIQ2::Algorithm::Impl::Impl(
     const std::string &fileName, const std::string &fileHash)
-    : Impl()
+    : initialized { false }
 {
 	// init RF module that takes some time to load the parameters
 	try {
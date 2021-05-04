3017a0a15b31d2f035964e869c5e9b03bd97e57a
aef1 <andrew.figlarz@nist.gov>
2021-05-04 08:26:21 -0400

    Add version header

--- NFIQ2/NFIQ2Algorithm/include/nfiq2_version.hpp
+++ NFIQ2/NFIQ2Algorithm/include/nfiq2_version.hpp
@@ -4,22 +4,31 @@
 #include <string>
 
 namespace NFIQ2 { namespace Version {
+
+/** Major version number. */
 extern const unsigned int Major;
+/** Minor version number. */
 extern const unsigned int Minor;
+/** Patch version number. */
 extern const unsigned int Patch;
 
+/** Full version information. */
 extern const std::string Full;
+/** Pretty version information. */
 extern const std::string Pretty;
+/** Build version status. */
 extern const std::string Status;
+/** Current build date. */
 extern const std::string BuildDate;
+/** Current build commit. */
 extern const std::string Commit;
 
-/*
- * Third-Party Components
- */
-
+/* Third-Party Components. */
+/** OpenCV version. */
 extern const std::string OpenCV;
+/** FingerJet version. */
 std::string FingerJet();
+
 } // namespace Version
 } // namespace NFIQ
 

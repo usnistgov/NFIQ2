72e188ac72c7bc2f6d7b0eb40bad18b04422ea0d
Greg Fiumara <gregory.fiumara@nist.gov>
2021-06-09 08:55:00 -0400

    Remove static map of strings from public API.
    Similar to previous changes that remove static C++ objects that maybe
    can't even be allocated.

--- NFIQ2/NFIQ2Algorithm/src/nfiq2/nfiq2_exception.cpp
+++ NFIQ2/NFIQ2Algorithm/src/nfiq2/nfiq2_exception.cpp
@@ -2,8 +2,35 @@
 
 NFIQ2::Exception::Exception(const NFIQ2::ErrorCode &errorCode)
     : errorCode(errorCode)
-    , errorMessage(errorCodeMessage.at(errorCode))
 {
+	/* Map of ErrorCode and their respective explanations. */
+	static const std::map<NFIQ2::ErrorCode, std::string> errorCodeMessage {
+		{ NFIQ2::ErrorCode::UnknownError, "Unknown error" },
+		{ NFIQ2::ErrorCode::NotEnoughMemory, "Not enough memory" },
+		{ NFIQ2::ErrorCode::BadArguments, "Bad arguments" },
+		{ NFIQ2::ErrorCode::FeatureCalculationError,
+		    "Feature calculation error" },
+		{ NFIQ2::ErrorCode::CannotWriteToFile, "Cannot write to file" },
+		{ NFIQ2::ErrorCode::CannotReadFromFile,
+		    "Cannot read from file" },
+		{ NFIQ2::ErrorCode::NoDataAvailable, "No data available" },
+		{ NFIQ2::ErrorCode::CannotDecodeBase64,
+		    "Cannot decode base64 string" },
+		{ NFIQ2::ErrorCode::InvalidConfiguration,
+		    "An invalid configuration entry was found" },
+		{ NFIQ2::ErrorCode::MachineLearningError,
+		    "An machine learning error occurred" },
+		{ NFIQ2::ErrorCode::FJFX_CannotCreateContext,
+		    "Cannot create context for FJFX feature extractor" },
+		{ NFIQ2::ErrorCode::FJFX_CannotCreateFeatureSet,
+		    "Cannot create feature set from fingerprint data" },
+		{ NFIQ2::ErrorCode::FJFX_NoFeatureSetCreated,
+		    "No feature set could be created" },
+		{ NFIQ2::ErrorCode::InvalidNFIQ2Score, "Invalid NFIQ2 Score" },
+		{ NFIQ2::ErrorCode::InvalidImageSize, "Invalid Image Size" }
+	};
+
+	this->errorMessage = errorCodeMessage.at(errorCode);
 }
 
 NFIQ2::Exception::Exception(

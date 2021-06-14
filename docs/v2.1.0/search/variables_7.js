697549048062f479aa7474e3ce56556e8ee0afa8
Greg Fiumara <gregory.fiumara@nist.gov>
2021-06-14 15:03:04 -0400

    Add v2.1.0 documentation

---
+++ docs/v2.1.0/search/variables_7.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['opencv_258',['OpenCV',['../namespace_n_f_i_q2_1_1_version.html#a5ad7d45e8463335164bad5a226781f64',1,'NFIQ2::Version']]],
+  ['orientationcertainty_259',['OrientationCertainty',['../namespace_n_f_i_q2_1_1_identifiers_1_1_quality_modules.html#a5e21a4995efdeeb5706ba60677d15b83',1,'NFIQ2::Identifiers::QualityModules']]],
+  ['orientationflow_260',['OrientationFlow',['../namespace_n_f_i_q2_1_1_identifiers_1_1_quality_modules.html#ae04af8797c9db24d93709c2fae2c7da9',1,'NFIQ2::Identifiers::QualityModules']]]
+];
---
+++ docs/v2.1.0/search/variables_d.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['width_275',['width',['../class_n_f_i_q2_1_1_fingerprint_image_data.html#a2a5bd8759440f3aed6e74f252c08fa40',1,'NFIQ2::FingerprintImageData']]]
+];

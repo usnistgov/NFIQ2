a580b3ac3a4334cf0551926f63e8e4c4b145d297
Greg Fiumara <gregory.fiumara@nist.gov>
2024-06-21 15:10:25 -0400

    Update registered CBEFF values.

--- NFIQ2/NFIQ2Algorithm/src/nfiq2/nfiq2_cbeff.cpp
+++ NFIQ2/NFIQ2Algorithm/src/nfiq2/nfiq2_cbeff.cpp
@@ -3,54 +3,54 @@
 /* ISO/IEC JTC 1 SC 37 Biometrics. */
 const unsigned int NFIQ2::Identifiers::CBEFF::Vendor { 0x0101 };
 
-const unsigned int NFIQ2::Identifiers::CBEFF::NFIQ2Rev0 { 0x01 };
-const unsigned int NFIQ2::Identifiers::CBEFF::NFIQ2Rev1 { 0x17 };
-const unsigned int NFIQ2::Identifiers::CBEFF::NFIQ2Rev2 { 0x18 };
+const unsigned int NFIQ2::Identifiers::CBEFF::NFIQ2Rev0 { 0x21 };
+const unsigned int NFIQ2::Identifiers::CBEFF::NFIQ2Rev1 { 0x37 };
+const unsigned int NFIQ2::Identifiers::CBEFF::NFIQ2Rev2 { 0x38 };
 const unsigned int NFIQ2::Identifiers::CBEFF::UnifiedQualityScore {
 	NFIQ2::Identifiers::CBEFF::NFIQ2Rev2
 };
 
 const unsigned int NFIQ2::Identifiers::CBEFF::OrientationCertainty::Mean {
-	0x02
+	0x22
 };
 const unsigned int NFIQ2::Identifiers::CBEFF::OrientationCertainty::StdDev {
-	0x03
+	0x23
 };
 
-const unsigned int NFIQ2::Identifiers::CBEFF::LocalClarity::Mean { 0x04 };
-const unsigned int NFIQ2::Identifiers::CBEFF::LocalClarity::StdDev { 0x05 };
+const unsigned int NFIQ2::Identifiers::CBEFF::LocalClarity::Mean { 0x24 };
+const unsigned int NFIQ2::Identifiers::CBEFF::LocalClarity::StdDev { 0x25 };
 
 const unsigned int NFIQ2::Identifiers::CBEFF::FrequencyDomainAnalysis::Mean {
-	0x06
+	0x26
 };
 const unsigned int NFIQ2::Identifiers::CBEFF::FrequencyDomainAnalysis::StdDev {
-	0x07
+	0x27
 };
 
 const unsigned int NFIQ2::Identifiers::CBEFF::RidgeValleyUniformity::Mean {
-	0x08
+	0x28
 };
 const unsigned int NFIQ2::Identifiers::CBEFF::RidgeValleyUniformity::StdDev {
-	0x09
+	0x29
 };
 
-const unsigned int NFIQ2::Identifiers::CBEFF::OrientationFlow::Mean { 0x0A };
-const unsigned int NFIQ2::Identifiers::CBEFF::OrientationFlow::StdDev { 0x0B };
+const unsigned int NFIQ2::Identifiers::CBEFF::OrientationFlow::Mean { 0x2A };
+const unsigned int NFIQ2::Identifiers::CBEFF::OrientationFlow::StdDev { 0x2B };
 
-const unsigned int NFIQ2::Identifiers::CBEFF::Contrast::Mean { 0x0C };
-const unsigned int NFIQ2::Identifiers::CBEFF::Contrast::MeanBlock { 0x0D };
+const unsigned int NFIQ2::Identifiers::CBEFF::Contrast::Mean { 0x2C };
+const unsigned int NFIQ2::Identifiers::CBEFF::Contrast::MeanBlock { 0x2D };
 
-const unsigned int NFIQ2::Identifiers::CBEFF::Minutiae::Count { 0x0E };
-const unsigned int NFIQ2::Identifiers::CBEFF::Minutiae::CountCOM { 0x0F };
-const unsigned int NFIQ2::Identifiers::CBEFF::Minutiae::QualityMu2 { 0x10 };
-const unsigned int NFIQ2::Identifiers::CBEFF::Minutiae::QualityOCL80 { 0x11 };
+const unsigned int NFIQ2::Identifiers::CBEFF::Minutiae::Count { 0x2E };
+const unsigned int NFIQ2::Identifiers::CBEFF::Minutiae::CountCOM { 0x2F };
+const unsigned int NFIQ2::Identifiers::CBEFF::Minutiae::QualityMu2 { 0x30 };
+const unsigned int NFIQ2::Identifiers::CBEFF::Minutiae::QualityOCL80 { 0x31 };
 
-const unsigned int NFIQ2::Identifiers::CBEFF::RegionOfInterest::Mean { 0x12 };
+const unsigned int NFIQ2::Identifiers::CBEFF::RegionOfInterest::Mean { 0x32 };
 const unsigned int NFIQ2::Identifiers::CBEFF::RegionOfInterest::CoherenceSum {
-	0x13
+	0x33
 };
 const unsigned int NFIQ2::Identifiers::CBEFF::RegionOfInterest::CoherenceMean {
-	0x14
+	0x34
 };
 
 const unsigned int NFIQ2::Identifiers::CBEFF::RadialPowerSpectrum { 0x15 };
